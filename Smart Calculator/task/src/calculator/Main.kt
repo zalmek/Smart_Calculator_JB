b5ae35c29f9c4ce45937094cb491f3c541c8fd5f
zalmek <zalmek23@gmail.com>
2022-08-21 20:34:19 +0300

    5 stage completed

--- Smart Calculator/task/src/calculator/Main.kt
+++ Smart Calculator/task/src/calculator/Main.kt
@@ -25,11 +25,21 @@ fun main() {
             }
 
             else -> {
-                inputLine=inputLine.filterNot {it == ' '}.replace("+"," ").replace("--"," ").replace("-"," -")
-                while (inputLine.replace("  "," ")!=inputLine){
-                    inputLine=inputLine.replace("  "," ")
+                inputLine = inputLine.filterNot { it == ' ' }
+                if (!inputLine.matches(Regex("\\s*[+-]*\\d+\\s*(\\s*[+-]+\\d+\\s*)*"))) {
+                    if (inputLine.contains(Regex("/"))) {
+                        println("Unknown command")
+                        continue
+                    }
+                    println("Invalid expression")
+                    continue
                 }
-                inputLine=inputLine.trim()
+                //println(inputLine)
+                inputLine = inputLine.replace("+", " ").replace("--", " ").replace("-", " -")
+                while (inputLine.replace("  ", " ") != inputLine) {
+                    inputLine = inputLine.replace("  ", " ")
+                }
+                inputLine = inputLine.trim()
                 val list: List<Int> = (inputLine.split(" ").map { it.toInt() })
                 println(sum(list))
             }
